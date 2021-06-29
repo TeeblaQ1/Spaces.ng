323ed8fc028e848e914eabad8ad5531d75d6168c
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2021-06-29 20:45:46 +0100

    Fixed admin.py file ordering issue

--- spaces/admin.py
+++ spaces/admin.py
@@ -4,9 +4,9 @@ from .models import Spaces
 # Register your models here.
 @admin.register(Spaces)
 class SpacesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'pricing', 'capacity', 'status')
+    list_display = ('name', 'slug', 'pricing', 'created', 'status')
     list_filter = ('status', 'created', 'capacity')
     search_fields = ('name', 'description')
     prepopulated_fields = {'slug': ('name',)}
     date_hierarchy = 'created'
-    ordering = ('status', 'capacity')
+    ordering = ('status', 'created')
