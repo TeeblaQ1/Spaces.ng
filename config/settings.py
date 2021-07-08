43874e2772d5daa68e9b35a7a8581f7243d5b78d
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2021-07-08 11:32:18 +0100

    Fixed logout modal and rest framework permissions

--- config/settings.py
+++ config/settings.py
@@ -219,7 +219,7 @@ CORS_ALLOW_CREDENTIALS = True
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
         ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
